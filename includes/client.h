7a84828461b891720e6c3d8d1cc68894cb9174e2
Jean-Daniel GRANET <jgranet@e1r8p13.42.fr>
2014-06-26 10:12:49 +0200

    ajout client.h

---
+++ includes/client.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/06/26 10:07:55 by jgranet           #+#    #+#             */
+/*   Updated: 2014/06/26 10:12:11 by jgranet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+typedef struct	s_stock
+{
+	int			nourriture;
+	int			linemate;
+	int			deraumere;
+	int			sibur;
+	int			mendiane;
+	int			phiras;
+	int			thystame;
+}				t_stock;
+
+typedef struct	s_client
+{
+	int			cs;
+	int			lvl;
+	int			nb_co;
+	t_stock		stock;
+}				t_client;
+
+void			ft_usage(void);
+void			ft_error(char *msg);
+
+#endif
