246a35bdd7bfe5a6c47ca7f127060e822c0d4fda
Jean-Daniel GRANET <jgranet@e1r8p13.42.fr>
2014-06-26 10:40:27 +0200

    truc

---
+++ srcs/client/ft_check_args.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_args.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/06/26 10:13:52 by jgranet           #+#    #+#             */
+/*   Updated: 2014/06/26 10:29:53 by jgranet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client.h"
+
+void			ft_fill_client(char **argv, int i, t_client *c)
+{
+	if (ft_strcmp(argv[i], "-n") == 0)
+		c->team = ft_strdup(argv[i + 1]);
+	else if (ft_strcmp(argv[i], "-p") == 0)
+		c->port = ft_atoi(argv[i + 1]);
+	else if (ft_strcmp(argv[i], "-h") == 0)
+		c->host = ft_strdup(argv[i + 1]);
+	else
+		ft_error("ERROR bad argument.");
+}
+
+void			ft_check_args(char **argv, t_client *c)
+{
+	int			i;
+
+	i = 1;
+	if (argv[i][0] != '-')
+		ft_error("ERROR bad argument.");
+	while (argv[i])
+	{
+		if (argv[i][0] == '-' && argv[i + 1] && argv[i + 1][0] != '-')
+		{
+			ft_fill_client(argv, i, c);
+			i += 2;
+		}
+		else
+			ft_error("ERROR bad argument.");
+	}
+}
--- srcs/client/main.c
+++ srcs/client/main.c
@@ -6,20 +6,62 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/26 09:50:50 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/26 10:05:44 by jgranet          ###   ########.fr       */
+/*   Updated: 2014/06/26 10:39:58 by jgranet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <signal.h>
 #include "client.h"
+#include "libft.h"
+
+void		ft_create_serveur(t_client *c)
+{
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+	struct hostent		*host;
+
+	if ((proto = getprotobyname("tcp")) == 0)
+		ft_error("ERROR getprotobyname");
+	if ((host = gethostbyname(c->host)) == NULL)
+		ft_error("ERROR gethostbyname");
+	if ((c->cs = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
+		ft_error("ERROR socket");
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(c->port);
+	ft_memcpy(&(sin.sin_addr.s_addr), host->h_addr, host->h_length);
+	if (connect(c->sock, (const struct sockaddr*)&sin, sizeof(sin)))
+		ft_error("ERROR connect");
+}
+
+void		ft_init_cli(t_client *c)
+{
+	c->cs = 0;
+	c->lvl = 1;
+	c->stock.nourriture = 0;
+	c->stock.linemate = 0;
+	c->stock.deraumere = 0;
+	c->stock.sibur = 0;
+	c->stock.mendiane = 0;
+	c->stock.phiras = 0;
+	c->stock.thystame = 0;
+	c->split = NULL;
+}
 
 int			main(int argc, char **argv)
 {
-	t_game		g;
+	t_client	c;
 	char		*line;
 	int			i;
 
 	signal(SIGINT, exit);
 	if (argc == 1)
 		ft_usage(argv[0]);
+	ft_check_args(argv, &c);
+	ft_create_serveur(&c);
+	return (0);
 }
