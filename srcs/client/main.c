8e350a9f20bdbbe6734194d32018a0a6dd10a94b
Jean-Daniel GRANET <jgranet@e1r8p13.42.fr>
2014-06-26 10:05:55 +0200

    modif main

--- srcs/client/main.c
+++ srcs/client/main.c
@@ -6,17 +6,12 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/26 09:50:50 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/26 09:57:43 by jgranet          ###   ########.fr       */
+/*   Updated: 2014/06/26 10:05:44 by jgranet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "client.h"
-
-void		ft_usage(char *argv)
-{
-	
-}
 
 int			main(int argc, char **argv)
 {
